c36657499f9ff27d34ad2d1590ceffe834369453
VladiVK <kozobon@gmail.com>
2022-04-25 14:39:18 +0300

    chore: setup temporary plugs for Pages

---
+++ src/pages/index.ts
@@ -0,0 +1,21 @@
+import AboutPage from './aboutPage/AboutPage';
+import AuthPage from './authPage/AuthWrapper';
+import CartPage from './cartPage/CartPage';
+import CheckoutPage from './checkoutPage/CheckoutPage';
+import ErrorPage from './errorPage/ErrorPage';
+import HomePage from './homePage/HomePage';
+import PrivateRoutePage from './privateroutePage/PrivateRoutePage';
+import ProductsPage from './productsPage/ProductsPage';
+import SingleProductPage from './singleProductPage/SingleProductPage';
+
+export {
+  AboutPage,
+  AuthPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoutePage,
+  ProductsPage,
+  SingleProductPage,
+};
---
+++ src/pages/privateroutePage/PrivateRoutePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PrivateRoutePage = () => {
+  return <div>PrivateRoutePage</div>;
+};
+
+export default PrivateRoutePage;
