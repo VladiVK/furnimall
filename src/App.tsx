5d81e194b2a07cca380369d21d2ea506fc4b4163
VladiVK <kozobon@gmail.com>
2022-06-05 17:40:54 +0300

    chore: add Link to "all products" to Featured items /home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useUserContext } from './context/user-context/user_context';
+// import { useUserContext } from './context/user-context/user_context';
 import { useAuth0 } from '@auth0/auth0-react';
 
 // Pages
